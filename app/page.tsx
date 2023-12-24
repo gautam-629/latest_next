1b8d37cb40d369f7feb460176ac872c83c338aad
gautam-629 <gautam629@gmail.com>
2023-12-24 22:27:04 +0545

    change fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import styles from "@/app/ui/home.module.css";
 import Link from "next/link";
-import { lusitana } from "@/app/ui/font";
+import { lusitana } from "@/app/ui/fonts";
 import Image from "next/image";
 export default function Page() {
   return (
